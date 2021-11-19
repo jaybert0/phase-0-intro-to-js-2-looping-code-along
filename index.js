01841a18580887b276d9a2f0d97e9b9c5e17b624
Jay Lee <jay.j.lee.1@gmail.com>
2021-11-19 18:40:19 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function wrapCards(names, event) {
+    const cardList = []
+    for (let i = 0; i < names.length; i++) {
+        cardList.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cardList;
+}
+
+wrapCards(["Reece", "Jay", "Penny"], "birthday");
+
+
+function countDown(x) {
+    let start = x;
+    while(start >= 0) {
+        console.log(start--);
+    }
+}
+countDown(10)
